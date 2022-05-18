11b7536488dfbaf86c80e26d1a5354439e210589
ningtenglei <ningtenglei@dongmengwl.com>
2022-05-18 17:20:06 +0800

    quack

---
+++ src/Hello/data.js
@@ -0,0 +1,22 @@
+export const tabsData = {
+    service:{
+        name: '服务器',
+        desc: "api.quack.io",
+        tabIconNormal:"/Common/img/tab/service_normal.png",
+        tabIconActive:"/Common/img/tab/service_active.png", 
+    },
+    history: {
+        name: '历史消息',
+        desc: "历史消息",
+        tabIconNormal:"/Common/img/tab/history_normal.png",
+        tabIconActive:"/Common/img/tab/history_active.png",
+
+    },
+    setting:{
+        name: '设置',
+        desc: "设置",
+        tabIconNormal:"/Common/img/tab/setting_normal.png",
+        tabIconActive:"/Common/img/tab/setting_active.png",
+    }
+  }
+  
